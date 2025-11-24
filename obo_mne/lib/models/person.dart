6991f961f140571bd184751d7b472d5d38e64e9e
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-11-24 18:06:23 +0000

    obo_mne

--- obo_mne/lib/models/person.dart
+++ obo_mne/lib/models/person.dart
@@ -21,6 +21,60 @@ class Person with _$Person {
   }) = _Person;
 
   factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+  
+  @override
+  // TODO: implement about
+  String get about => throw UnimplementedError();
+  
+  @override
+  // TODO: implement age
+  int get age => throw UnimplementedError();
+  
+  @override
+  // TODO: implement avatarUrl
+  String get avatarUrl => throw UnimplementedError();
+  
+  @override
+  // TODO: implement education
+  String get education => throw UnimplementedError();
+  
+  @override
+  // TODO: implement email
+  String get email => throw UnimplementedError();
+  
+  @override
+  // TODO: implement experience
+  String get experience => throw UnimplementedError();
+  
+  @override
+  // TODO: implement id
+  int get id => throw UnimplementedError();
+  
+  @override
+  // TODO: implement name
+  String get name => throw UnimplementedError();
+  
+  @override
+  // TODO: implement phone
+  String get phone => throw UnimplementedError();
+  
+  @override
+  // TODO: implement position
+  String get position => throw UnimplementedError();
+  
+  @override
+  // TODO: implement skills
+  List<String> get skills => throw UnimplementedError();
+  
+  @override
+  // TODO: implement surname
+  String get surname => throw UnimplementedError();
+  
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
 }
 
 extension PersonExtension on Person {
